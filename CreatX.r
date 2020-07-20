55b7751ca23e37eb105a18ac7eddb3b90dd096b6
LaiJiang <jianglai1987@gmail.com>
2020-07-20 11:00:41 -0400

    Update CreatX.r

--- CreatX.r
+++ CreatX.r
@@ -1,28 +1,21 @@
 
-
-
 rm(list=ls())
 
 library(xgboost)
 library(mlr)
 library(pROC)
 #######################################################
-path.main <- "/mnt/GREENWOOD_JBOD1/GREENWOOD_BACKUP/home/lai.jiang/Brent/Github/" 
-  
-path.dat <- paste0(path.main,"dat/")
 
-load(paste0(path.dat,"annot.RData"),verbose=TRUE)
-
-path.func <- paste0(path.main,"func")
-
+#load your annottion data raw.annot
+load("annot.RData",verbose=TRUE)
 
 test.annot <- raw.annot
 
 test.traits <- unique(test.annot$trait)
 
-
 snp.dat <- test.annot
 
+#process features
 snp.dat$gene.length <- abs(snp.dat$gene.chrom.end - snp.dat$gene.chrom.start) 
 snp.dat$snp.tss.dist <- abs(snp.dat$snp.tss.dist)
 snp.dat$snp.gene.dist <- abs(snp.dat$snp.gene.dist)
@@ -33,7 +26,7 @@ snp.dat$z <- abs(snp.dat$z)
 ###########################################################################
 ###########################################################################
 
-source( paste0(path.func, "/func_summary.r" )   )
+source(  "func_summary.r"  )
 
 all.func.names <- c(lsf.str())
 all.func.names <- all.func.names[grep("fn.",all.func.names)]  
